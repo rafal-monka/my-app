13cee851b6be0888812691f662b0ce8ab5a678c9
Rafal Monka <monka.rafal@gmail.com>
2020-03-31 14:47:08 +0200

    Updates

---
+++ precache-manifest.b6da9a316a2d1f34a29921bf5cffcd7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f02fa6d9b1aa633b05a0232c83739f5",
+    "url": "/my-app/build/index.html"
+  },
+  {
+    "revision": "7cd010d3190d53707438",
+    "url": "/my-app/build/static/css/main.cdbab6c4.chunk.css"
+  },
+  {
+    "revision": "c9a791c33144c12ea1cd",
+    "url": "/my-app/build/static/js/2.0da06914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/build/static/js/2.0da06914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd010d3190d53707438",
+    "url": "/my-app/build/static/js/main.2ff7217f.chunk.js"
+  },
+  {
+    "revision": "094440cd38a64d0beb2d",
+    "url": "/my-app/build/static/js/runtime-main.558a867c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
